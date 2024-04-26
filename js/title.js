bba558c78fac40d11b38e1f255cf6425aa914377
Egor Andreev <110353893+ItzVenskiy@users.noreply.github.com>
2024-04-26 19:49:42 +0200

    Delete js directory

--- js/title.js
+++
@@ -1,1 +0,0 @@
-var message=(message="ItzVenskiy | Главная")+" ";i="0";var temptitle="",speed="200";function titler(){(document.all||document.getElementById)&&(document.title=temptitle+message.charAt(i),temptitle+=message.charAt(i),i++,i==message.length&&(i="0",temptitle=""),setTimeout("titler()",speed))}window.onload=titler;
